a5a0950eb20dccb709b27eb134ea04f5c0d4ba24
AppaProgramming <appa.programming@gmail.com>
2017-02-07 20:53:28 +0000

    ADD StringHelper class ADD full behaviour of challenge to console

---
+++ solution/WordCounter/WordCounter/Helper/StringHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter.Helper
+{
+    public static class StringHelper
+    {
+        internal static string[] SplitWordsFromText(string textFromFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Dictionary<string, int> GetWordCount(string[] words)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static List<string> GetLinesOfTextToDisplay(Dictionary<string, int> wordCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- solution/WordCounter/WordCounter/Program.cs
+++ solution/WordCounter/WordCounter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WordCounter.Helper;
 
 namespace WordCounter
 {
@@ -12,10 +13,17 @@ namespace WordCounter
         {
             // Read the file as one string.
             string textFromFile = System.IO.File.ReadAllText(@"..\..\FilesToRead\Test1.txt");
+            // Get each word.
+            string[] words = StringHelper.SplitWordsFromText(textFromFile);
+            // Word count
+            Dictionary<string, int> wordCount = StringHelper.GetWordCount(words);
+            // 
+            List<string> textToDisplay = StringHelper.GetLinesOfTextToDisplay(wordCount);
 
-
-
-            Console.WriteLine(textFromFile);
+            foreach (var line in textToDisplay)
+            {
+                Console.WriteLine(line);
+            }
             Console.ReadLine();
         }
     }
